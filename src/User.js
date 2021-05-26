efd189641deca559320de36c33e343c5b909fc04
Bs1987 <boysax@gmail.com>
2021-05-26 12:09:07 +0200

    Add score state to User component

--- src/User.js
+++ src/User.js
@@ -1,5 +1,23 @@
+import {useState} from "react";
+
 function User (props) {
-	console.log("USERNAME IS: ", props);
-	return <div>{props.username}</div>
+	// score: getter current state of "score"
+	// setScore: setter. function to update score
+	// =useState(0) => initialise state at initial state (start at setter "0")
+	//useState is a hook: starts with "use"
+
+	const [score, setScore] = useState(0);
+	const whatIsThis = useState("bla");
+	console.log("WHAT IS THIS: ", whatIsThis);
+
+	return (
+	 <div>
+		 {props.username}: {score}
+		 <button onClick={() => setScore(score + 1)}>+</button>
+		 <button onClick={() => setScore(score - 1)}>-</button>
+
+	 </div>
+	);
 }
+
 export default User;
