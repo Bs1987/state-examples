84aaa60226db9de1da33bf6b8998faeb5b74fba2
Bs1987 <boysax@gmail.com>
2021-05-29 11:32:11 +0200

    render form conditionally

--- src/User.js
+++ src/User.js
@@ -7,30 +7,40 @@ function User (props) {
 	//useState is a hook: starts with "use"
 
 	const [score, setScore] = useState(0);
-	// const whatIsThis = useState("bla");
-	// console.log("WHAT IS THIS: ", whatIsThis);
 	const [name, setName] = useState("Boy");
 	const [avatar, setAvatar] = useState("😮")
+	const [editMode, setEditmote] = useState(false);
+
+	//onclick
+	// formulier laten verdwijnen
+	//boolean , true of false -> conditional render
+	//submit -> false
+	//pennetje -> true
+
 
 	return (
 	 <div>
-		 <div>
+		 {editMode && <div>
 			 <label>Playername </label>
-			 <input value={name} onChange={(event) => setName(event.target.value)}/>
-			 <button>Submit</button>
-			 <select value={avatar} onChange={(event) => setAvatar(event.target.value)}>
+			 <input
+			  value={name}
+			  onChange={(event) => setName(event.target.value)}
+			 />
+			 <button onClick={() => setEditmote(false)}>Submit</button>
+			 <select
+			  value={avatar}
+			  onChange={(event) => setAvatar(event.target.value)}
+			 >
 				 <option>😁</option>
 				 <option>🙀</option>
 				 <option>😍</option>
 				 <option>😎</option>
 			 </select>
-		 </div>
-		 {avatar} {name}: {score}
-		 {/*{ score === 10 && <p>You have won!!</p>}*/}
-		 {/*{score ==s*/}
+		 </div>}
+		 {avatar} - {name}: {score}
 		 <button onClick={() => setScore(score + 1)}>+</button>
 		 <button onClick={() => setScore(score - 1)}>-</button>
-		 <button>📝</button>
+		 <button onClick={() => setEditmote(!editMode)}>📝</button>
 	 </div>
 	);
 }
